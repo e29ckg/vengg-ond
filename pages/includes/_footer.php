cb039d34e4858c03d40449598639a79179b0d1c0
e29ckg <e29ckg@gmail.com>
2022-12-05 10:39:32 +0700

    206-8

--- pages/includes/_footer.php
+++ pages/includes/_footer.php
@@ -1,13 +1,12 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted d-print-none">
         <div class="float-start">
-            <p>2022 &copy; Pkkjc - V 2.0.6
+            <p>2022 &copy; Pkkjc - V 2.0.6 
                 <a href="http://sites.google.com/view/pkkjc-ven-v1" target="_blank" class="footer-link fw-bolder">คู่มือ</a>
             </p>
         </div>
         <div class="float-end">
-            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                    href="http://ahmadsaugi.com" target="_blank">A. Saugi</a></p>
+            <p> <?='Current PHP version: ' . phpversion();?> </p>
         </div>
     </div>
 </footer>
